0e7c081f69d3687de7c4b91f0b3bfaa6e5234979
Marcin <skorekmarcin@gmail.com>
2021-03-22 21:29:59 +0100

    feat: prettier setup

--- src/Form/Form.tsx
+++ src/Form/Form.tsx
@@ -1,10 +1,19 @@
-import React from 'react';
+import { Formik } from 'formik';
 import { FormWrapper } from './Form.styles';
 
 export const Form = () => {
   return (
     <FormWrapper>
-      Sample form
+      <Formik
+        initialValues={
+          {
+            title: ''
+          }
+        }
+        onSubmit={(values) => console.log(values)}
+      >
+        
+      </Formik>
     </FormWrapper>
   )
 };
