01fb92e04e5d1b83496bb4cdeefd74bc5900f830
Marcin <skorekmarcin@gmail.com>
2021-03-23 19:51:26 +0100

    feat: initial text component

--- src/Form/Form.tsx
+++ src/Form/Form.tsx
@@ -1,4 +1,6 @@
-import { Formik } from 'formik';
+import { Field, Formik } from 'formik';
+import React from 'react';
+import { FormikTextField } from '../shared/FormikTextField';
 import { FormWrapper } from './Form.styles';
 
 export const Form = () => {
@@ -12,7 +14,12 @@ export const Form = () => {
         }
         onSubmit={(values) => console.log(values)}
       >
-        
+        <Field
+          name="title"
+          variant="outlined"
+          fullWidth
+          component={FormikTextField}
+        />
       </Formik>
     </FormWrapper>
   )
---
+++ src/shared/FormikTextField/FormikTextField.tsx
@@ -0,0 +1,59 @@
+// import get from 'lodash/get';
+import {TextField, TextFieldProps } from "@material-ui/core";
+import { FieldInputProps, FieldMetaProps, FormikProps } from 'formik';
+
+// TODO: figure out generics, in formik files it's any as well
+interface FormikTextFieldProps {
+  field: FieldInputProps<any>;
+  form: FormikProps<any>;
+  meta: FieldMetaProps<any>;
+  type: TextFieldProps;
+}
+
+export const FormikTextField = ({
+  field,
+  meta,
+  form,
+  type,
+  // decoratorStart,
+  // decoratorEnd,
+  // precision,
+  ...otherProps
+}: FormikTextFieldProps) => {
+  // const isInvalid = get(form.errors, field.name) && (get(form.touched, field.name) || form.submitCount > 0);
+  // const handleKeyPress = (evt:any) => {
+  //   if (
+  //     otherProps.type === "number" &&
+  //     ["e", "E", "+", "-"].includes(evt.key)
+  //   ) {
+  //     evt.preventDefault();
+  //   }
+  // };
+
+  return (
+    <>
+      <TextField
+        {...field}
+        {...otherProps}
+        name={field.name}
+        id={field.name}
+        margin="dense"
+        value={field.value}
+        onChange={form.handleChange}
+        // error={isInvalid}
+        // type={type}
+        autoComplete="off"
+        // helperText={isInvalid && get(form.errors, field.name)}
+        // InputProps={{
+        //   onKeyPress: handleKeyPress,
+        //   startAdornment: decoratorStart && (
+        //     <InputAdornment position="start">{decoratorStart}</InputAdornment>
+        //   ),
+        //   endAdornment: decoratorEnd && (
+        //     <InputAdornment position="end">{decoratorEnd}</InputAdornment>
+        //   )
+        // }}
+      />
+    </>
+  );
+};
